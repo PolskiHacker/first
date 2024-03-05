b2050e09aa632bfc7c873a4b4b0ea16a65dc1a91
kulis <fshkfshf@fjfj.com>
2024-03-05 14:28:31 +0300

    05.03 version

--- parsing.py
+++ parsing.py
@@ -5,18 +5,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 s = Service('C:/Program Files/chromedriver-win64/chromedriver.exe')
 browser = webdriver.Chrome(service=s)
-# browser.get("https://www.kinopoisk.ru/lists/movies/top250/?utm_referrer=www.kinopoisk.ru")
-# time.sleep(5)
-# html_text = browser.page_source
-# soup = BeautifulSoup(html_text, 'lxml')
-# div = soup.find('div', class_= 'base-movie-main-info_mainInfo__ZL_u3').find('span', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj').text
-# span = soup.find('a', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj').get('hfa')
-# span_2=soup.find('span', class_='desktop-list-main-info_secondaryText__M_aus').text
-# finding = soup.find_all('span', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj')
-# print(span)
-# print(span_2),lp,
-# print(finding[0].text)
-'''a = "https://www.kinopoisk.ru/lists/movies/top250/?utm_referrer=www.kinopoisk.ru&page="
+'''browser.get("https://www.kinopoisk.ru/lists/movies/top250/?utm_referrer=www.kinopoisk.ru")
+time.sleep(5)
+html_text = browser.page_source
+soup = BeautifulSoup(html_text, 'lxml')
+div = soup.find('div', class_= 'base-movie-main-info_mainInfo__ZL_u3').find('span', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj').text
+span = soup.find('a', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj').get('hfa')
+span_2=soup.find('span', class_='desktop-list-main-info_secondaryText__M_aus').text
+finding = soup.find_all('span', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj')
+print(span)
+print(span_2),lp,
+print(finding[0].text)
+a = "https://www.kinopoisk.ru/lists/movies/top250/?utm_referrer=www.kinopoisk.ru&page="
 data = []
 browser.get(a + '1')
 time.sleep(4)
